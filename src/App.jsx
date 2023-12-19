43b92c24cb97b2c9a78fc66e5b4c4850d24b3216
olhakvl <olhakoval21@gmail.com>
2023-12-18 21:28:05 -0600

    set up a table with the test contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
 
+
+
   return (
     <div>
-      
+      <ContactList />
     </div>
   )
 }
